776f5eeadbbc521747b6e3110356d0d041c5c979
Andres Giraldo <andresfgiraldo@live.com>
2019-10-26 15:38:24 -0500

    configurar CORS | cap. 84

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.use(bodyParser.json()); //convertir todas las peticiones que lleguen a JSON
 
 
 // CORS - activar para permitir peticiones desde frontend
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 
 // Prefijos a rutas
